0a691526bca4d4eaf9e49b56e066adfc68bc3a5e
Anh Le <lehoanganhhd@gmail.com>
2023-09-28 11:33:12 +0700

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditComponent } from './members/photo-edit/photo-edit.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditComponent
   ],
   imports: [
     BrowserModule,
