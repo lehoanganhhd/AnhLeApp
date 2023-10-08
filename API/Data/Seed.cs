4dbb40e9eb6ebd63a8ce61ba27639e2703add156
Anh Le <lehoanganhhd@gmail.com>
2023-10-08 10:37:26 +0700

    Pre-deployment

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,5 +1,3 @@
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using API.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +7,12 @@ namespace API.Data
 {
     public class Seed
     {
+        public static async Task ClearConnection(DataContext context)
+        {
+            context.Connections.RemoveRange(context.Connections);
+            await context.SaveChangesAsync();
+        }
+
         public static async Task SeedUsers(UserManager<AppUser> userManager,
             RoleManager<AppRole> roleManager)
         {
@@ -34,7 +38,9 @@ namespace API.Data
 
             foreach (var user in users)
             {
-                user.UserName = user.UserName.ToLower();                
+                user.UserName = user.UserName.ToLower();
+                user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+                user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);               
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
             }
